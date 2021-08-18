e7099e4cc1de5aa75ebab9e668de7442bfbefed9
dufftie <dufftie@gmail.com>
2021-08-18 19:22:33 +0300

    Migration added:
    -Added migration for users table

---
+++ database/migrations/2021_08_06_094702_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('password');
+            $table->string('name');
+            $table->string('surname');
+            $table->tinyInteger('sex');
+            $table->string('email');
+            $table->string('profileImage');
+            $table->text('description');
+            $table->string('location');
+            $table->string('links');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
